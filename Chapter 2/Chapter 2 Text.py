540590c7de0451dd6d91218d0657c56671e389c6
Elena NARDI <elena.nardi@oldi.fr>
2022-03-14 16:41:40 +0100

    2.3.2

--- Chapter 2/Chapter 2 Text.py
+++ Chapter 2/Chapter 2 Text.py
@@ -39,24 +39,55 @@ class CreditCard:
         self._balance -= amount
         
         
-if __name__ == "__main__":
-    wallet = []
-    wallet.append(CreditCard("John Bowman", "California Savings", "5391 0375 9387 5309", 2500))
-    wallet.append(CreditCard("John Bowman", "California Federal", "3485 0399 3395 1954", 3500))
-    wallet.append(CreditCard("John Bowman", "California Finance", "5391 0375 9387 5309", 5000))
+# if __name__ == "__main__":
+#     wallet = []
+#     wallet.append(CreditCard("John Bowman", "California Savings", "5391 0375 9387 5309", 2500))
+#     wallet.append(CreditCard("John Bowman", "California Federal", "3485 0399 3395 1954", 3500))
+#     wallet.append(CreditCard("John Bowman", "California Finance", "5391 0375 9387 5309", 5000))
     
-    for val in range(1,17):
-        wallet[0].charge(val)
-        wallet[1].charge(2*val)
-        wallet[2].charge(3*val)
+#     for val in range(1,17):
+#         wallet[0].charge(val)
+#         wallet[1].charge(2*val)
+#         wallet[2].charge(3*val)
     
-    for c in range(3):
-        print("Customer =", wallet[c].get_customer())
-        print("Bank =", wallet[c].get_bank())
-        print("Account =", wallet[c].get_account())
-        print("Limit =", wallet[c].get_limit())
-        print("Balance =", wallet[c].get_balance())
-        while wallet[c].get_balance() > 100:
-            wallet[c].make_payement(100)
-            print("New balance =", wallet[c].get_balance())
-        print()
+#     for c in range(3):
+#         print("Customer =", wallet[c].get_customer())
+#         print("Bank =", wallet[c].get_bank())
+#         print("Account =", wallet[c].get_account())
+#         print("Limit =", wallet[c].get_limit())
+#         print("Balance =", wallet[c].get_balance())
+#         while wallet[c].get_balance() > 100:
+#             wallet[c].make_payement(100)
+#             print("New balance =", wallet[c].get_balance())
+#         print()
+
+class Vector:
+    def __init__(self, d):
+        self._coords = [0]*d
+        
+    def __len__(self):
+        return len(self._coords)
+    
+    def __getitem__(self, j):
+        return self._coords[j]
+    
+    def __setitem__(self, j, val):
+        self._coords[j] = val
+        
+    def __add__(self, other):
+        if len(self) != len(other):
+            raise ValueError("dimensions must agree")
+        result = Vector(len(self))
+        for j in range(len(self)):
+            result[j] = self[j] + other[j]
+        return result
+    
+    def __eq__(self, other):
+        return self._coords == other._coords
+    
+    def __ne__(self, other):
+        return not self == other
+    
+    def __str__(self):
+        return "<" + str(self._coords)[1:-1] + ">"
+            
