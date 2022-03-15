190e5fd49d22d62cc2832e32d7b52965f13cdf82
Elena NARDI <elena.nardi@oldi.fr>
2022-03-15 13:13:33 +0100

    2.3

--- Chapter 2/Chapter 2 Text.py
+++ Chapter 2/Chapter 2 Text.py
@@ -90,4 +90,78 @@ class Vector:
     
     def __str__(self):
         return "<" + str(self._coords)[1:-1] + ">"
+    
+    
+class SequenceIterator:
+    
+    
+    def __init__(self, sequence):
+        self._seq = sequence
+        self._k = -1
+        
+    def __next__(self):
+        self._k += 1
+        if self._k < len(self._seq):
+            return(self._seq[self._k])
+        else:
+            raise StopIteration()
             
+    def __iter__(self):
+        return self
+    
+    
+class Range:
+    def __init__(self, start, stop = None, step =1):
+        if step == 0: 
+            raise ValueError("Step cannot be 0")
+        if stop is None:
+            start, stop = 0, start
+            
+        self._length = max(0, (stop-start+step-1)//step)
+        self._start = start
+        self._step = step
+        
+        
+        def __len__(self):
+            return self._length
+        
+        def __getitem__(self, k):
+            if k < 0:
+                k += len(self)
+            if not 0 <= k <= self._length:
+                raise IndexError("Index out of range")
+            return self._start + k*self._step
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
